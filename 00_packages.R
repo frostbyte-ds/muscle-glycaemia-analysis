98b0537a2a5ea6251faa4dc8e4dc515bc2bbd24f
James Frost <j.a.frost@proton.me>
2025-07-20 17:51:02 +0100

    Adding code to facilitate the installation of the required packages

--- 00_packages.R
+++ 00_packages.R
@@ -2,25 +2,44 @@
 # Loading required packages for the analysis
 # ------------------------------------------ #
 
-library(nhanesA) # For NHANEs data
+# If you need to install any of the packages first, uncomment the following
+# code and run it
+# install.packages("nhanesA") 
+# install.packages("survey") 
+# install.packages("systemfonts") 
+# install.packages("showtext") 
+# install.packages("Amelia") 
+# install.packages("parallel") 
+# install.packages("mice") 
+# install.packages("mitools") 
+# install.packages("svydiags")
+# install.packages("poliscidata") 
+# install.packages("tidyverse") 
+# install.packages("heiscore")
+# install.packages("car") 
+# install.packages("svydiags") 
+# install.packages("miceadds") 
+# install.packages("gt") 
+# install.packages("arm") 
+# install.packages("gridExtra") 
+# install.packages("WeightedROC") 
+
+library(nhanesA) # For NHANES data
 library(survey) # For incorporation of survey design elements
 library(systemfonts) # For fonts
 library(showtext) # Also for fonts
-library(VIM) # For testing for multicollinearity 
 library(Amelia) # For multiple imputation
 library(parallel) # For parallel processing
 library(mice) # For more imputation tools
 library(mitools) # For even MORE imputation tools
-library(broom)
-library(nnet) # For multinomial regression
 library(svydiags) # For survey model diagnostics
 library(poliscidata) # For fit.svyglm function
 library(tidyverse) # For tidyverse functions
 library(heiscore) # For HEI scores
 library(car) # For useful analysis tools
-library(svydiags)
+library(svydiags) # For diagnostics of svyglms
 library(miceadds) # For micombine.F
 library(gt) # For tables
-library(arm)
+library(arm) # For binned residual plots
 library(gridExtra) # For presentation of visualisations
 library(WeightedROC) # For survey weighted ROC and AUC calculations 
